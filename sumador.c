63dee76d78ee640759ed674a5e01b2cef51760ce
Daniel Boyan R. <daniel.boyan@alu.inspt.utn.edu.ar>
2024-04-18 00:02:11 -0300

    Sumador V1.4

--- sumador.c
+++ sumador.c
@@ -5,7 +5,7 @@
 
 int main (void)
 {
-    int num1,num2,num3,suma;
+    int num1,num2,num3,num4,suma;
 
     p("Ingrese Numero 1:");
     s("%d",&num1);
@@ -16,10 +16,13 @@ int main (void)
 
     p("Ingrese Numero 3:");
     s("%d",&num3);
+    
+    p("Ingrese Numero 4:");
+    s("%d",&num4);
 
 
-    suma = num1 + num2 + num3;
+    suma = num1 + num2 + num3 + num4;
 
-    p("La suma de %d + %d + %d = %d",num1,num2,num3,suma);
+    p("La suma de %d + %d + %d  + %d = %d",num1,num2,num3,num4,suma);
     return 0;
 }
